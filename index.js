36ade7d78dc9271898faa11fad52994c2b1ff182
Tanveer <tanveeransari5663@gmail.com>
2025-06-16 15:35:18 +0530

    third

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
+app.get("/api/get", (req, res) => {
+  res.send({ message: "hello back to nodejs" });
+});
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("Server is running on http://localhost:3000");
 });
